9476a506f73c4fd99c154d0b3b72562332a1d444
Yuancheng Zhang <old.far.hot@gmail.com>
2016-03-29 13:11:14 -0500

    added hailstone

---
+++ 1_1_hailstone/hailstone/hailstone/hailstone.cpp
@@ -0,0 +1,29 @@
+﻿// hailstone.cpp : 为了验证的算法的有穷性
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int hailstone(int);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	for (int i = 42; i < 43; i++)
+	{
+		cout << i << ':' << hailstone(i) << endl;
+	}
+
+	return 0;
+}
+
+int hailstone(int n)
+{
+	int length = 1;
+	while (n > 1)
+	{
+		n % 2 ? n = n * 3 + 1 : n /= 2; 
+		length++;
+	}
+	return length;
+}
